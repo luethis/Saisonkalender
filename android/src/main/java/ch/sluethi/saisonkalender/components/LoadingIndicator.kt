6442f16cd4ed986f7548eb2cc8cee302140f29c7
Serafin Lüthi <serafin.luethi@outlook.com>
2022-07-10 16:38:43 +0200

    add preview to LoadingIndicator.kt

--- android/src/main/java/ch/sluethi/saisonkalender/components/LoadingIndicator.kt
+++ android/src/main/java/ch/sluethi/saisonkalender/components/LoadingIndicator.kt
@@ -1,12 +1,27 @@
 package ch.sluethi.saisonkalender.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ch.sluethi.saisonkalender.ui.theme.SaisonkalenderTheme
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun LoadingIndicatorPreview() {
+    SaisonkalenderTheme {
+        Surface {
+            LoadingIndicator(isDisplayed = true)
+        }
+    }
+}
 
 @Composable
 fun LoadingIndicator(isDisplayed: Boolean) {
